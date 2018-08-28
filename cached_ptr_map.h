d73ee68a604d22e45e35e8ebe709fa1d191ba3d6
jk-jeon <jk_jeon@kaist.ac.kr>
2018-08-29 01:52:47 +0900

    Fixed wrong indentation

--- cached_ptr_map.h
+++ cached_ptr_map.h
@@ -52,122 +52,122 @@ namespace jkl {
 			}
 		};
 
-		public:
-			// Launch the cleaner thread
-			cached_ptr_map()
-			{
-				cleaner_ = std::async(std::launch::async, [this]() {
-					cleaner_thread();
-				});
-			}
+	public:
+		// Launch the cleaner thread
+		cached_ptr_map()
+		{
+			cleaner_ = std::async(std::launch::async, [this]() {
+				cleaner_thread();
+			});
+		}
 
-			// Terminate the cleaner thread
-			~cached_ptr_map()
+		// Terminate the cleaner thread
+		~cached_ptr_map()
+		{
+			// Signal to the cleaner thread
 			{
-				// Signal to the cleaner thread
+				std::lock_guard<jkl::shared_mutex> lg{ mtx_ };
+				terminate_ = true;
+			}
+			cv_.notify_one();
+
+			// Wait
+			cleaner_.get();	// should not throw
+		}
+
+		// Perform a sort of double-checked locking
+		template <class Query, class... InitialArgs>
+		std::shared_ptr<Value> get(Query&& q, InitialArgs&&... args)
+		{
+			// Called when a new object is to be created
+			auto make_new_object = [&]() {
+				// The deleter tells the cleaner thread to erase the weak_ptr from the map
+				return std::shared_ptr<Value>(new Value(std::forward<InitialArgs>(args)...),
+					[this](Value* ptr)
 				{
-					std::lock_guard<jkl::shared_mutex> lg{ mtx_ };
-					terminate_ = true;
-				}
-				cv_.notify_one();
-
-				// Wait
-				cleaner_.get();	// should not throw
-			}
-
-			// Perform a sort of double-checked locking
-			template <class Query, class... InitialArgs>
-			std::shared_ptr<Value> get(Query&& q, InitialArgs&&... args)
-			{
-				// Called when a new object is to be created
-				auto make_new_object = [&]() {
-					// The deleter tells the cleaner thread to erase the weak_ptr from the map
-					return std::shared_ptr<Value>(new Value(std::forward<InitialArgs>(args)...),
-						[this](Value* ptr)
+					// Signal to the cleaner thread
 					{
-						// Signal to the cleaner thread
-						{
-							std::lock_guard<jkl::shared_mutex> lg{ mtx_ };
-							something_to_clean_ = true;
-						}
-						cv_.notify_one();
-
-						// Delete the object
-						delete ptr;
-					});
-				};
-
-				// Return value
-				// This object should be cleared after the lock is released,
-				// because the destructor might perform locking.
-				std::shared_ptr<Value> ptr;
-
-				// This is called if there is no cache found in the map
-				auto emplace_new = [&](auto& lg) {
-					// Obtain write lock
-					upgrade_lock_guard lg2{ *lg.release() };
-
-					// Find the key again, because the map might have been modified
-					auto itr = map_.find(q);
-
-					// If the key still does not exist,
-					if( itr == map_.end() ) {
-						// Make a new object and put it in the map
-						ptr = make_new_object();
-						map_.emplace(q, ptr);
-						return ptr;
+						std::lock_guard<jkl::shared_mutex> lg{ mtx_ };
+						something_to_clean_ = true;
 					}
-					// Otherwise
-					else {
-						// If the pointer is expired, recreate the entry
-						ptr = itr->second.lock();
-						if( !ptr ) {
-							ptr = make_new_object();
-							itr->second = ptr;
-						}
-						return ptr;
-					}
-				};
+					cv_.notify_one();
 
-				// Obtain read-lock
-				std::shared_lock<jkl::shared_mutex>	lg{ mtx_ };
+					// Delete the object
+					delete ptr;
+				});
+			};
 
-				// If the key is not in the map
+			// Return value
+			// This object should be cleared after the lock is released,
+			// because the destructor might perform locking.
+			std::shared_ptr<Value> ptr;
+
+			// This is called if there is no cache found in the map
+			auto emplace_new = [&](auto& lg) {
+				// Obtain write lock
+				upgrade_lock_guard lg2{ *lg.release() };
+
+				// Find the key again, because the map might have been modified
 				auto itr = map_.find(q);
+
+				// If the key still does not exist,
 				if( itr == map_.end() ) {
-					return emplace_new(lg);
+					// Make a new object and put it in the map
+					ptr = make_new_object();
+					map_.emplace(q, ptr);
+					return ptr;
 				}
 				// Otherwise
 				else {
-					// If the pointer is expired
+					// If the pointer is expired, recreate the entry
 					ptr = itr->second.lock();
-					if( !ptr )
-						return emplace_new(lg);
-					// Otherwise
-					else
-						return ptr;
-				}
-			}
-
-		private:
-			// Erase expired weak_ptr's from the map
-			void cleaner_thread() {
-				while( true ) {
-					// Wait for signal
-					std::unique_lock<jkl::shared_mutex> lg{ mtx_ };
-					cv_.wait(lg, [this]() { return something_to_clean_ || terminate_; });
-
-					if( terminate_ )
-						break;
-
-					// Erase expired weak_ptr's
-					for( auto itr = map_.begin(); itr != map_.end(); ) {
-						if( itr->second.expired() )
-							map_.erase(itr++);
-						else
-							++itr;
+					if( !ptr ) {
+						ptr = make_new_object();
+						itr->second = ptr;
 					}
+					return ptr;
+				}
+			};
+
+			// Obtain read-lock
+			std::shared_lock<jkl::shared_mutex>	lg{ mtx_ };
+
+			// If the key is not in the map
+			auto itr = map_.find(q);
+			if( itr == map_.end() ) {
+				return emplace_new(lg);
+			}
+			// Otherwise
+			else {
+				// If the pointer is expired
+				ptr = itr->second.lock();
+				if( !ptr )
+					return emplace_new(lg);
+				// Otherwise
+				else
+					return ptr;
+			}
+		}
+
+	private:
+		// Erase expired weak_ptr's from the map
+		void cleaner_thread() {
+			while( true ) {
+				// Wait for signal
+				std::unique_lock<jkl::shared_mutex> lg{ mtx_ };
+				cv_.wait(lg, [this]() { return something_to_clean_ || terminate_; });
+
+				if( terminate_ )
+					break;
+
+				// Erase expired weak_ptr's
+				for( auto itr = map_.begin(); itr != map_.end(); ) {
+					if( itr->second.expired() )
+						map_.erase(itr++);
+					else
+						++itr;
 				}
 			}
+		}
 	};
 }
